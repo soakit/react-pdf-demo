6f98636b65cc96c1e5376929500e07b86bd0fffe
soakit <liuchang2024@gmail.com>
2025-06-23 11:55:43 +0800

    fix: 更新package.json和README.md，调整启动和构建脚本
    - 在package.json中添加NODE_OPTIONS以解决openssl兼容性问题
    - 更新README.md中的字体测试说明，改善内容可读性
    - 规范化代码格式，确保一致性

--- README.md
+++ README.md
@@ -1,26 +1,38 @@
+# 启动
+
+```shell
+npm i
+npm run start
+```
+
 # 试验字体
 
 ## SourceHanSansSC-Medium_dehint2.002.1.ttf
 
-pdf内容展示ok
+pdf 内容展示 ok
 
 ## SourceHanSansSC-Medium-Min.ttf
 
-pdf内容展示不全，大部分字展示不了。
+pdf 内容展示不全，大部分字展示不了。
 
 ## lite_all_chars_part.ttf
 
-pdf内容展示ok。基于`SourceHanSansSC-Medium_dehint2.002.1.ttf`和`lite_all_chars_part.txt`。
+pdf 内容展示 ok。基于`SourceHanSansSC-Medium_dehint2.002.1.ttf`和`lite_all_chars_part.txt`。
 
 ```shell
-pyftsubset "d:\workspace\react-pdf-demo\src\font\SourceHanSansSC-Medium_dehint2.002.1.ttf" --text-file="d:\workspace\react-pdf-demo\lite_all_chars_part.txt" --output-file="d:\workspace\react-pdf-demo\src\font\lite_all_chars_part.ttf"
+pyftsubset "src/font/SourceHanSansSC-Medium_dehint2.002.1.ttf" --text-file="lite_all_chars_part.txt" --output-file="src/font/lite_all_chars_part.ttf"
 ```
-但是给`lite_all_chars_part.txt`增加任意新的字符，都会导致pdf内容展示不全。
+
+但是给`lite_all_chars_part.txt`增加任意新的字符，都会导致 pdf 内容展示不全。
 
 ## lite_all_chars.ttf
 
-pdf内容展示不全，大部分字展示不了。基于`SourceHanSansSC-Medium_dehint2.002.1.ttf`和`lite_all_chars.txt`。
+pdf 内容展示不全，大部分字展示不了。基于`SourceHanSansSC-Medium_dehint2.002.1.ttf`和`lite_all_chars.txt`。
 
 ```shell
-pyftsubset "d:\workspace\react-pdf-demo\src\font\SourceHanSansSC-Medium_dehint2.002.1.ttf" --text-file="d:\workspace\react-pdf-demo\lite_all_chars.txt" --output-file="d:\workspace\react-pdf-demo\src\font\lite_all_chars.ttf"
+pyftsubset "src/font/SourceHanSansSC-Medium_dehint2.002.1.ttf" --text-file="lite_all_chars.txt" --output-file="src/font/lite_all_chars.ttf"
 ```
+
+## spider_lite_all_chars.ttf
+
+pdf 内容展示 ok。但是无法 letterSpacing（pdf 换行）。
--- src/MyDocument.js
+++ src/MyDocument.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Page, Text, View, Document, StyleSheet, Font } from "@react-pdf/renderer";
+import {
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  Font,
+} from "@react-pdf/renderer";
 
 // 会缺字
 // import SiYuanHeiTiMedium from "./font/SourceHanSansSC-Medium-Min.ttf";
@@ -30,7 +37,7 @@ Font.register({
 const styles = StyleSheet.create({
   page: {
     // flexDirection: "row",
-    backgroundColor: "#ffffff"
+    backgroundColor: "#ffffff",
   },
   section: {
     // margin: 10,
@@ -43,82 +50,102 @@ const styles = StyleSheet.create({
     // justifyContent: "center",
     // flexDirection: "row",
     // backgroundColor: "#ffffff",
-  }
+  },
 });
 
 // Create Document Component
 const MyDocument = () => {
   return (
     <Document>
-      <Page size="A4" style={{
-        ...styles.page,
-        fontFamily: "siyuanheiti"
-      }}
+      <Page
+        size="A4"
+        style={{
+          ...styles.page,
+          fontFamily: "siyuanheiti",
+        }}
 
-      // debug
-      // size={pageInfo.pageSize}
-      // ruler={debug}
-      // style={[
-      //   { fontFamily: fontCode ? FontCodeMap[fontCode] || 'han' : 'han' },
-      //   pageInfo.pagePadding
-      // ]}
-
+        // debug
+        // size={pageInfo.pageSize}
+        // ruler={debug}
+        // style={[
+        //   { fontFamily: fontCode ? FontCodeMap[fontCode] || 'han' : 'han' },
+        //   pageInfo.pagePadding
+        // ]}
       >
         <View>
-          <Text style={{
-            fontSize: 12,
-            textAlign: "left",
-            letterSpacing: -5.5,
-            lineHeight: 1.5,
-            margin: 10
-          }}>销 售 订 单 往 来 位 名 称 制 时 间 收 货 地 址 北 京 市 东 城 区 西 长 安 街 天 门 楼 人 尘 序 号</Text>
-
+          <Text
+            style={{
+              fontSize: 12,
+              textAlign: "left",
+              letterSpacing: -5.5,
+              lineHeight: 1.5,
+              margin: 10,
+            }}
+          >
+            销 售 订 单 往 来 位 名 称 制 时 间 收 货 地 址 北 京 市 东 城 区 西
+            长 安 街 天 门 楼 人 尘 序 号
+          </Text>
         </View>
-        
+
         {/* 测试不同的 letterSpacing 值 */}
         <View>
-          <Text style={{
-            fontSize: 12,
-            textAlign: "left",
-            letterSpacing: -10.5,
-            lineHeight: 1.5,
-            margin: 10
-          }}>销 售 订 单 往 来 位 名 称 制 时 间 收 货 地 址 北 京 市 东 城 区 西 长 安 街 天 门 楼 人 尘 序 号</Text>
-
-        </View>
-        
-        <View>
-          <Text style={{
-            fontSize: 12,
-            textAlign: "left",
-            letterSpacing: -1.5,
-            lineHeight: 1.5,
-            margin: 10
-          }}>销 售 订 单 往 来 位 名 称 制 时 间 收 货 地 址 北 京 市 东 城 区 西 长 安 街 天 门 楼 人 尘 序 号</Text>
-
+          <Text
+            style={{
+              fontSize: 12,
+              textAlign: "left",
+              letterSpacing: -10.5,
+              lineHeight: 1.5,
+              margin: 10,
+            }}
+          >
+            销 售 订 单 往 来 位 名 称 制 时 间 收 货 地 址 北 京 市 东 城 区 西
+            长 安 街 天 门 楼 人 尘 序 号
+          </Text>
         </View>
 
         <View>
-          <Text style={{
-            fontSize: 12,
-            width: "100%",
-            textAlign: "left",
-            lineHeight: 1.5,
-            margin: 10
-          }}>商品编码规格数量单位价金额小计备注比例辅助循环满减分类圾多件个包</Text>
+          <Text
+            style={{
+              fontSize: 12,
+              textAlign: "left",
+              letterSpacing: -1.5,
+              lineHeight: 1.5,
+              margin: 10,
+            }}
+          >
+            销 售 订 单 往 来 位 名 称 制 时 间 收 货 地 址 北 京 市 东 城 区 西
+            长 安 街 天 门 楼 人 尘 序 号
+          </Text>
         </View>
 
         <View>
-          <Text style={{
-            fontSize: 12,
-            fontFamily: "siyuanheiti",
-            width: "100%",
-            textAlign: "left",
-            lineHeight: 1.5,
-            margin: 10
-          }}>无运费应付大写叁佰玖拾柒元角肆送日期蓝色JulyCAxyhgml0123456789:：/-.</Text>
+          <Text
+            style={{
+              fontSize: 12,
+              width: "100%",
+              textAlign: "left",
+              lineHeight: 1.5,
+              margin: 10,
+            }}
+          >
+            商品编码规格数量单位价金额小计备注比例辅助循环满减分类圾多件个包
+          </Text>
         </View>
 
+        <View>
+          <Text
+            style={{
+              fontSize: 12,
+              fontFamily: "siyuanheiti",
+              width: "100%",
+              textAlign: "left",
+              lineHeight: 1.5,
+              margin: 10,
+            }}
+          >
+            无运费应付大写叁佰玖拾柒元角肆送日期蓝色JulyCAxyhgml0123456789:：/-.
+          </Text>
+        </View>
       </Page>
     </Document>
   );
